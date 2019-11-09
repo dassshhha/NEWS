e6435083234145b8a643f2171ac2097e323c6659
dassshhha <55977442+dassshhha@users.noreply.github.com>
2019-11-09 19:30:48 +0300

    Add files via upload

---
+++ WebS/views.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from WebS import app
+
+
+@app.route('/')
+@app.route('/mainPage')
+def show_words():
+    # words = []
+    lines=[]
+    with open(str('data/mainNews.txt'), 'r', encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line[:-2]
+            lines.append(line)
+    return render_template('mainPage.html',
+                           lines=lines)
+
